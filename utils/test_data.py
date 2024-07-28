bb0f9de926eb6d4383cc86c337b6101356f01cb3
pradeep7676 <pradepreddy1994@gmail.com>
2024-07-28 11:35:37 +0530

    updated testdata

--- utils/test_data.py
+++ utils/test_data.py
@@ -1,5 +1,5 @@
 USER_DATA = {
-    "valid": {"username": "liran12@1", "password": "123456"},
+    "valid": {"username": "kiran12@12", "password": "123123"},
     "existing": {"existing_username": "wings", "existing_password": "123123"},
     "invalid": {"username": "invaliduser", "password": "wrongpass"},
     "specialcha" : {"spcecial_characters_username": "prad1dp@1e1p@23424", "special_char_Password": "1@23@3&%$4567"}
